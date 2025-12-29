de04035bd43a8d853530c1e6bd14e0ef75b0c978
Joaomacosdev <joamarcosdev@gmail.com>
2025-12-29 14:59:03 -0300

    inicializando o projeto

---
+++ src/test/java/br/com/portalconcurso/portalaprovados/PortalaprovadosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.portalconcurso.portalaprovados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalaprovadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
